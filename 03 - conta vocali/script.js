0a41f989e09f45d7a21598bd0b905b23e059cc0b
ilariamotta <ilaria.motta93@gmail.com>
2025-11-04 16:52:10 +0100

    feat: esercizio 3 completato

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,20 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function contaVocali(parola) {
+    let vocali = ["a", "e", "i", "o", "u"];
+    let numeroVocali = 0;
+    for (let i = 0; i < parola.length; i++) {
+        for (let j = 0; j < vocali.length; j++)
+            if (parola.charAt(i) === vocali[j]) {
+                numeroVocali++;
+            }
+    }
+    return numeroVocali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(contaVocali(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
