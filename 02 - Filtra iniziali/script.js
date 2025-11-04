0cdd1f8db0f867dda211348ef3eb9ba96b8a6383
ilariamotta <ilaria.motta93@gmail.com>
2025-11-04 16:17:12 +0100

    feat: esercizio 2 completato

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,9 +5,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function iniziaCon(array, lettera) {
+    let letterCap = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].charAt(0) === lettera) {
+            letterCap.push(array[i]);
+        }
+    }
+    return letterCap;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(iniziaCon(names, "A"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
